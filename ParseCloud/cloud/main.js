6db362a969332d2205f8c9e005181a3bd6cd2ec8
Jason Fieldman <jason@fieldman.org>
2015-03-28 01:02:19 -0700

    updates to cloud code

--- ParseCloud/cloud/main.js
+++ ParseCloud/cloud/main.js
@@ -1,6 +1,25 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.beforeSave("Snap", function(request, response) {
+	
+	var user  = request.user;
+	var taker = request.object.get("taker");
+	
+	if (user.objectId !== taker.objectId) {
+		response.error("Taker must be the current user");
+	} else {
+		response.success();
+	}	
+});
+
+
+Parse.Cloud.beforeSave("SentSnap", function(request, response) {
+	
+	var user  = request.user;
+	var taker = request.object.get("taker");
+	
+	if (user.objectId !== taker.objectId) {
+		response.error("Taker must be the current user");
+	} else {
+		response.success();
+	}	
 });
