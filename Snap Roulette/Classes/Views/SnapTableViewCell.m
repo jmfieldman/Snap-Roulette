a2e4e9873c3e15bd9e2453da8c565e895650aa56
Jason Fieldman <jason@fieldman.org>
2015-04-14 14:38:59 -0700

    added some labeling stuff

--- Snap Roulette/Classes/Views/SnapTableViewCell.m
+++ Snap Roulette/Classes/Views/SnapTableViewCell.m
@@ -44,6 +44,11 @@
         _takerImageView.layer.shouldRasterize = YES;
         _takerImageView.layer.rasterizationScale = [UIScreen mainScreen].scale;
         [self.contentView addSubview:_takerImageView];
+        
+        _takerNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(52, 10, 200, 32)];
+        _takerNameLabel.font = [UIFont fontWithName:@"Lato Regular" size:14];
+        _takerNameLabel.textColor = [UIColor colorWithWhite:0.1 alpha:1];
+        [self.contentView addSubview:_takerNameLabel];
     }
     return self;
 }
@@ -60,8 +65,17 @@
     _snap = snap;
     
     /* Taker */
+    PFUser *taker = snap[@"taker"];
+    
     _takerImageView.image = [UIImage imageNamed:@"facebook_default_portrait"];
-    [_takerImageView sd_setImageWithURL:[NSURL URLWithString:[RandomHelpers urlForFBPicture:snap[@"taker"]]] placeholderImage:[UIImage imageNamed:@"facebook_default_portrait"] options:0 completed:nil];
+    [_takerImageView sd_setImageWithURL:[NSURL URLWithString:[RandomHelpers urlForFBPicture:taker]] placeholderImage:[UIImage imageNamed:@"facebook_default_portrait"] options:0 completed:nil];
+    
+    //_takerNameLabel.text = taker[@"fullname"];
+    
+    NSMutableAttributedString *namestr = [[NSMutableAttributedString alloc] initWithString:taker[@"fullname"] attributes:@{ NSFontAttributeName : [UIFont boldSystemFontOfSize:14] }];
+    [namestr appendAttributedString:[[NSAttributedString alloc] initWithString:@" " attributes:@{}]];
+    [namestr appendAttributedString:[[NSAttributedString alloc] initWithString:@"10m ago" attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:10], NSForegroundColorAttributeName : [UIColor blueColor] }]];
+    _takerNameLabel.attributedText = namestr;
     
     /* Snap */
     _snapImageView.image = nil;
