e4812c8d9c6df1c5c8df2f34fd4b41333216034f
Jason Fieldman <jason@fieldman.org>
2015-04-14 12:56:24 -0700

    adding snap table cell code

---
+++ Snap Roulette/Classes/Views/SnapTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  SnapTableViewCell.h
+//  Snap Roulette
+//
+//  Created by Jason Fieldman on 4/14/15.
+//  Copyright (c) 2015 Jason Fieldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SnapTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) PFObject *snap;
+
+@end
---
+++ Snap Roulette/Classes/Views/SnapTableViewCell.m
@@ -0,0 +1,30 @@
+//
+//  SnapTableViewCell.m
+//  Snap Roulette
+//
+//  Created by Jason Fieldman on 4/14/15.
+//  Copyright (c) 2015 Jason Fieldman. All rights reserved.
+//
+
+#import "SnapTableViewCell.h"
+
+@interface SnapTableViewCell (Private)
+
+@property (nonatomic, strong) UIImageView *snapImageView;
+
+@end
+
+
+@implementation SnapTableViewCell
+
+- (id) initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
+        
+        
+        
+    }
+    return self;
+}
+
+
+@end
